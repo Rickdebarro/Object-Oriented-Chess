2ce412ff9840d8bbf7299d41cf2ecc2cf6dfc289
LucasMota10 <lucasbulcaobmota@gmail.com>
2024-05-25 19:20:25 -0300

    Queen's Move

--- ChessProject/src/Pieces/Queen.java
+++ ChessProject/src/Pieces/Queen.java
@@ -1,4 +1,6 @@
 package Pieces;
+import java.util.ArrayList;
+
 import Board.Board;
 import generalElements.*;
 
@@ -11,7 +13,160 @@ public class Queen extends Piece { // classe dos rainha, herda os atributos e me
     
     @Override
     public void SetPossible_Pos(Board tab,int x, int y){
+
+        movi_possibilityX = new ArrayList();
+ 	    movi_possibilityY = new ArrayList();
+
     	System.out.println("\u001B[31m" + "Você selecionou um RAINHA!" + "\u001B[0m");
+        System.out.println(RED + "As Movimentações possiveis estão marcadas em amarelo!" + ANSI_RESET);
         
+        int auxX = x, auxY = y;
+
+        while(auxX+1 <= 7 && auxY+1 <= 7){
+            if(tab.board[auxX+1][auxY+1].getIsFree()){
+                movi_possibilityX.add(auxX+1);
+                movi_possibilityY.add(auxY+1);
+            }else{
+                if(tab.board[x][y].getPiece().getColor() != tab.board[auxX+1][auxY+1].getPiece().getColor()){
+                    movi_possibilityX.add(auxX+1);
+                    movi_possibilityY.add(auxY+1);
+                    break;
+                }else{
+                    break;
+                }
+            }
+
+            auxX++;
+            auxY++;
+        }
+
+        auxX = x; auxY = y;
+
+        while(auxX+1 <=7 && auxY-1 >= 0){
+            if(tab.board[auxX+1][auxY-1].getIsFree()){
+                movi_possibilityX.add(auxX+1);
+                movi_possibilityY.add(auxY-1);
+            }else{
+                if(tab.board[x][y].getPiece().getColor() != tab.board[auxX+1][auxY-1].getPiece().getColor()){
+                    movi_possibilityX.add(auxX+1);
+                    movi_possibilityY.add(auxY-1);
+                    break;
+                }else{
+                    break;
+                }
+            }
+
+            auxX++;
+            auxY--;
+        }
+
+        auxX = x; auxY = y;
+
+        while(auxX-1 >= 0 && auxY+1 <= 7){
+            if(tab.board[auxX-1][auxY+1].getIsFree()){
+                movi_possibilityX.add(auxX-1);
+                movi_possibilityY.add(auxY+1);
+            }else{
+                if(tab.board[x][y].getPiece().getColor() != tab.board[auxX-1][auxY+1].getPiece().getColor()){
+                    movi_possibilityX.add(auxX-1);
+                    movi_possibilityY.add(auxY+1);
+                    break;
+                }else{
+                    break;
+                }
+            }
+
+            auxX--;
+            auxY++;
+        }
+
+        auxX = x; auxY = y;
+
+        while(auxX-1 >= 0 && auxY-1 >= 0){
+            if(tab.board[auxX-1][auxY-1].getIsFree()){
+                movi_possibilityX.add(auxX-1);
+                movi_possibilityY.add(auxY-1);
+            }else{
+                if(tab.board[x][y].getPiece().getColor() != tab.board[auxX-1][auxY-1].getPiece().getColor()){
+                    movi_possibilityX.add(auxX-1);
+                    movi_possibilityY.add(auxY-1);
+                    break;
+                }else{
+                    break;
+                }
+            }
+
+            auxX--;
+            auxY--;
+        }
+
+        int dir = y,esq = y,cima = x,baixo = x;
+
+    while(esq-1 >= 0){
+        if(tab.board[x][esq-1].getIsFree()){
+            movi_possibilityX.add(x);
+            movi_possibilityY.add(esq-1);
+        }else{
+            if(tab.board[x][esq-1].getPiece().getColor() != tab.board[x][y].getPiece().getColor()){
+                movi_possibilityX.add(x);
+                movi_possibilityY.add(esq-1);
+                break;
+            }else{
+                break;
+            }
+        }
+        esq--;
+    }
+
+    while(dir+1 <= 7){
+        if(tab.board[x][dir+1].getIsFree()){
+            movi_possibilityX.add(x);
+            movi_possibilityY.add(dir+1);
+        }else{
+            if(tab.board[x][dir+1].getPiece().getColor() != tab.board[x][y].getPiece().getColor()){
+                movi_possibilityX.add(x);
+                movi_possibilityY.add(dir+1);
+                break;
+            }else{
+                break;
+            }
+        }
+
+        dir++;
+    }
+
+    while(baixo+1 <= 7){
+        if(tab.board[baixo+1][y].getIsFree()){
+            movi_possibilityX.add(baixo+1);
+            movi_possibilityY.add(y);
+        }else{
+            if(tab.board[baixo+1][y].getPiece().getColor() != tab.board[x][y].getPiece().getColor()){
+                movi_possibilityX.add(baixo+1);
+                movi_possibilityY.add(y);
+                break;
+            }else{
+                break;
+            }
+        }
+        baixo++;
+    }
+
+    while(cima-1 >= 0){
+        if(tab.board[cima-1][y].getIsFree()){
+            movi_possibilityX.add(cima-1);
+            movi_possibilityY.add(y);
+        }else{
+            if(tab.board[cima-1][y].getPiece().getColor() != tab.board[x][y].getPiece().getColor()){
+                movi_possibilityX.add(cima-1);
+                movi_possibilityY.add(y);
+                break;
+            }else{
+                break;
+            }
+        }
+        cima--;
+    }  
+
+    tab.Print_Board_Possibility(movi_possibilityX, movi_possibilityY);
     }
 }
